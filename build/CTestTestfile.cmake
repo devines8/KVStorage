42b1abb7d8d75ad56b706827bcc273c708c4c09d
Anton <devines8@list.ru>
2025-08-15 18:18:54 +0300

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devines/vk_test_assignment/Storage
-# Build directory: /home/devines/vk_test_assignment/Storage/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(kv_storage_tests "/home/devines/vk_test_assignment/Storage/build/kv_storage_tests")
-set_tests_properties(kv_storage_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/devines/vk_test_assignment/Storage/CMakeLists.txt;37;add_test;/home/devines/vk_test_assignment/Storage/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
